e6e12261b1cb04f2edc37e8709497a2eeac3160e
saraa <saraalmongui@gmail.com>
2026-05-26 16:58:15 -0500

    primer commit

---
+++ backend/servicio-historial/src/main/java/edu/uptc/swii/servicio_historial/ServicioHistorialApplication.java
@@ -0,0 +1,13 @@
+package edu.uptc.swii.servicio_historial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioHistorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicioHistorialApplication.class, args);
+	}
+
+}
---
+++ backend/servicio-historial/src/test/java/edu/uptc/swii/servicio_historial/ServicioHistorialApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.uptc.swii.servicio_historial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioHistorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
