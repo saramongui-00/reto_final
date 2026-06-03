886877f49911d621899597e6fb9c7c024e5e66ce
saraa <saraalmongui@gmail.com>
2026-06-03 07:57:20 -0500

    antes de cambiar a develop

--- backend/servicio-citas/src/main/java/edu/uptc/swii/servicio_citas/domain/model/Cita.java
+++ backend/servicio-citas/src/main/java/edu/uptc/swii/servicio_citas/domain/model/Cita.java
@@ -8,9 +8,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
-/*
-* citas
-* */
+
 public class Cita {
 
     private final String id;
