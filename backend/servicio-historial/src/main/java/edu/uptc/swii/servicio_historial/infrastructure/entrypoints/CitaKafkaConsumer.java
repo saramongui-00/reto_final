41a139473c5b8b9e09a52fc969de64cef822baf5
saraa <saraalmongui@gmail.com>
2026-06-03 07:12:36 -0500

    quitar logs de kafka

--- backend/servicio-historial/src/main/java/edu/uptc/swii/servicio_historial/infrastructure/entrypoints/CitaKafkaConsumer.java
+++ backend/servicio-historial/src/main/java/edu/uptc/swii/servicio_historial/infrastructure/entrypoints/CitaKafkaConsumer.java
@@ -2,10 +2,8 @@ package edu.uptc.swii.servicio_historial.infrastructure.entrypoints;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import edu.uptc.swii.servicio_historial.infrastructure.adapters.SpringDataCitasEnEsperaRepository;
 import edu.uptc.swii.servicio_historial.infrastructure.adapters.CitaEnEsperaEntity;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import edu.uptc.swii.servicio_historial.infrastructure.adapters.SpringDataCitasEnEsperaRepository;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 import java.time.LocalDateTime;
@@ -13,8 +11,7 @@ import java.time.LocalDateTime;
 @Component
 public class CitaKafkaConsumer {
 
-    // Cambiamos los Sysout por un Logger Estándar de SLF4J
-    private static final Logger log = LoggerFactory.getLogger(CitaKafkaConsumer.class);
+
 
     private final SpringDataCitasEnEsperaRepository citasEnEsperaRepository;
     private final ObjectMapper objectMapper;
@@ -27,8 +24,6 @@ public class CitaKafkaConsumer {
     @KafkaListener(topics = "cita-lista-atencion", groupId = "historial-service-lean-group")
     public void listen(String eventJson) {
         try {
-            // Esto aparecerá en tu consola sí o sí con el formato de Spring
-            log.info("📥 [KAFKA RECEPTOR] Llegó un mensaje desde el topic: {}", eventJson);
 
             JsonNode root = objectMapper.readTree(eventJson);
 
@@ -41,7 +36,6 @@ public class CitaKafkaConsumer {
                     root.get("pacienteId");
 
             if (idNode == null || idNode.isNull() || patientNode == null || patientNode.isNull()) {
-                log.error("⚠️ [KAFKA ERROR] Estructura incompatible. No se detectó ID de cita o paciente en: {}", eventJson);
                 return;
             }
 
@@ -49,10 +43,9 @@ public class CitaKafkaConsumer {
             String patientId = patientNode.asText();
 
             citasEnEsperaRepository.save(new CitaEnEsperaEntity(citaId, patientId, LocalDateTime.now()));
-            log.info("✅ [KAFKA ÉXITO] Cita '{}' guardada en MongoDB local de Historial.", citaId);
 
         } catch (Exception e) {
-            log.error("❌ Error crítico procesando el evento de Kafka: {}", e.getMessage(), e);
+
         }
     }
 }
