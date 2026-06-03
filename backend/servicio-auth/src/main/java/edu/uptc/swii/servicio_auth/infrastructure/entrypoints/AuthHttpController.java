9d16fd9cf38c6e7b48680a97cfe5bb5014964d2d
Diana Saavedra <diana.ojeda01@uptc.edu.co>
2026-06-02 21:58:17 -0500

    add facebook login

--- backend/servicio-auth/src/main/java/edu/uptc/swii/servicio_auth/infrastructure/entrypoints/AuthHttpController.java
+++ backend/servicio-auth/src/main/java/edu/uptc/swii/servicio_auth/infrastructure/entrypoints/AuthHttpController.java
@@ -1,10 +1,12 @@
 package edu.uptc.swii.servicio_auth.infrastructure.entrypoints;
 
 import edu.uptc.swii.servicio_auth.application.dto.AuthResponseDto;
+import edu.uptc.swii.servicio_auth.application.dto.FacebookLoginRequestDto;
 import edu.uptc.swii.servicio_auth.application.dto.LoginRequestDto;
 import edu.uptc.swii.servicio_auth.application.dto.RegisterRequestDto;
 import edu.uptc.swii.servicio_auth.application.ports.LoginUseCase;
 import edu.uptc.swii.servicio_auth.application.ports.RegisterUseCase;
+import edu.uptc.swii.servicio_auth.application.ports.FacebookAuthUseCase;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,10 +17,13 @@ public class AuthHttpController {
 
     private final RegisterUseCase registerUseCase;
     private final LoginUseCase loginUseCase;
+    private final FacebookAuthUseCase facebookAuthUseCase;
 
-    public AuthHttpController(RegisterUseCase registerUseCase, LoginUseCase loginUseCase) {
+    public AuthHttpController(RegisterUseCase registerUseCase, LoginUseCase loginUseCase,
+            FacebookAuthUseCase facebookAuthUseCase) {
         this.registerUseCase = registerUseCase;
         this.loginUseCase = loginUseCase;
+        this.facebookAuthUseCase = facebookAuthUseCase;
     }
 
     @PostMapping("/register")
@@ -41,6 +46,16 @@ public class AuthHttpController {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.UNAUTHORIZED);
         } catch (RuntimeException e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.FORBIDDEN);
+        }
+    }
+
+    @PostMapping("/facebook")
+    public ResponseEntity<?> loginWithFacebook(@RequestBody FacebookLoginRequestDto requestDto) {
+        try {
+            AuthResponseDto response = facebookAuthUseCase.execute(requestDto);
+            return ResponseEntity.ok(response);
+        } catch (RuntimeException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.UNAUTHORIZED);
         }
     }
 }
